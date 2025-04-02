2190ab3a35e55024eae49c25977aa96560c977d2
Auriiiel <aurielgr@gmail.com>
2025-04-03 00:23:19 +0400

    Script+matrice de départ

--- Main_code.py
+++ Main_code.py
@@ -1,4 +1,14 @@
-import matplotlib as plt
+import matplotlib.pyplot as plt
 import numpy as np
+import random
 
-
+def perco(n, p):
+    grille = []
+    for i in range(n):
+        ligne = []  
+        for j in range(n):
+            if random.random() < p:
+                ligne.append(0) 
+            else:
+                ligne.append(1) 
+        grille.append(ligne) 
