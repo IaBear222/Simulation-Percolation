f28fbd3c81841e51262fa83e6e1d665c886466cd
Auriiiel <aurielgr@gmail.com>
2025-04-03 15:46:53 +0400

    Add perco function

--- Main_code.py
+++ Main_code.py
@@ -3,12 +3,32 @@ import numpy as np
 import random
 
 def perco(n, p):
-    grille = []
+    grille = np.zeros((n,n))
     for i in range(n):
-        ligne = []  
         for j in range(n):
             if random.random() < p:
-                ligne.append(0) 
+                grille[i][j]=0
             else:
-                ligne.append(1) 
-        grille.append(ligne) 
+                grille[i][j]=1
+    return grille 
+
+def visu(grille):
+
+    visu = np.zeros((n, n))
+
+
+    for i in range(n):
+        for j in range(n):
+            if grille[i][j] == 1:
+                visu[i][j] = 1 
+            elif grille[i][j] == 0:
+                visu[i][j] = 0  
+            elif grille[i][j] == 2:
+                visu[i][j] = 0.5
+
+    
+    plt.imshow(visual_grid, cmap='gray')
+    plt.colorbar(label='0 = libre, 1 = fermé, 2 = visitée')
+    plt.title("grille de percolation ")
+    plt.show()
+
