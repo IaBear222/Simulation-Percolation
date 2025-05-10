cb83dc6e5c3a23fd76563adca3f71ff0c16c0880
Auriiiel <aurielgr@gmail.com>
2025-05-10 18:37:31 +0400

    Update running_file.py

--- running_file.py
+++ running_file.py
@@ -2,57 +2,37 @@ from libraries import *
 from plot import *
 from find_a_path import *
 
-
-n = 50
-p = 0.5
-
-
-def system(n, p):
-    """
-    this function creates a random system of size n x n with a probability p of having a 1 in each cell.
-    The system is surrounded by a wall of 1s to modelize the system borders.
-
-    >>> input :
-    n : the size of the system
-    p : the probability of having a 1 in each cell wich means having matter
-
-    <<< output:
-    grille : the system created
-    """
-    grille = np.ones((n + 2, n + 2))
-    for i in range(1, n + 1):
-        for j in range(1, n + 1):
-
-            grille[i][j] = np.random.choice([1, 0], p=[p, 1 - p])
-
-    return grille
-
+n = 500
+p = 0.75
+repet= 10
 
 def rdm_syst(n, p):
-    syst = system(n, p)
-    the_wall(syst)
-    the_line(syst)
-    _, syst = alt(syst)
+    """
+    Function to create a random system of size n with a probability p and verify if there is a direct path or if there is a wall
+    """
+    syst = system(n, p) #generate the system
+    the_wall(syst) #check if there is a wall
+    the_line(syst)#check if there is a direct line
+
     plot_system(syst, p)
 
 
-rdm_syst(n, p)
+def run_boucle(n, p, repet): 
+    """
+     Function to create a random system of size n with a probability p and generate a path in the system and if there is a path, then it will print the number of paths found and the percolation rate
+    """
+    compteur = 0  #initialization of the counter
 
+    for _ in range(repet): #to repeat the process a certain number of times
+        syst = system(n, p) #generate the system
+        path, syst = alt(syst) #generate the path in the system
+        if path == 1: #check if there is a path
+            compteur += 1 #increment the counter
 
-def run_boucle(n, p, repet):
-    """ """
-    compteur = 0
-
-    for _ in range(repet):
-        syst = system(n, p)
-        path, syst = alt(syst)
-        if path == 1:
-            compteur += 1
-
-    Taux = 100 * compteur / repet
+    Taux = 100 * compteur / repet #calculate the percolation rate
 
     print(f"\nNombre de chemins trouvés : {compteur} sur {repet}")
     print(f"Taux de percolation : {Taux:.2f}%")
 
 
-run_boucle(n, p, 5)
+run_boucle(n, p, repet)
